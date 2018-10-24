a838c989ba59e9f9b55b3b82ac2fef3ed00723c5
marc.lyon <marc.lyon@qa.com>
2018-10-24 14:20:39 +0100

    Added Test Commands

---
+++ Test_commands.ps1
@@ -0,0 +1,21 @@
+﻿
+
+### Create a Random String
+$rand = -join ((65..90) + (97..122) | get-random -Count 5  | % {[char]$_})
+write-host $rand
+
+
+### Create a Directory
+$dir_exists = Test-Path 'C:\Users\MarcLyon\Desktop\Powershell'
+
+if ($dir_exists -ne 'True'){
+    New-item -Path 'C:\Users\MarcLyon\Desktop\Powershell' -ItemType Directory
+}
+
+
+### Create a File
+$dir_exists = Test-Path 'C:\Users\MarcLyon\Desktop\Powershell'
+
+if ($dir_exists -ne 'True'){
+    New-item -Path 'C:\Users\MarcLyon\Desktop\Powershell\Powershell-File'
+}
